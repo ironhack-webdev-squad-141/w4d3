8bda8dee6b11de60529e3a4995405febac9151e3
mjarraya <mjarraya@student.42.fr>
2019-06-26 17:27:58 +0200

    nba-app

---
+++ nba-app/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/players", (req, res) => {
+  res.render("players");
+});
+
+app.get("/teams", (req, res) => {
+  res.render("teams");
+});
+
+app.listen(3000);
