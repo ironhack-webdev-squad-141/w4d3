a323a7c49d9babc3d5ffd85da8e4540503a0c44a
mjarraya <mjarraya@student.42.fr>
2019-06-26 12:46:49 +0200

    stylesheet

--- express-server/app.js
+++ express-server/app.js
@@ -1,9 +1,9 @@
-/*
-const express = require('express')
-const app = express()
-*/
+const express = require("express");
+const app = express();
 
-const app = require("express")();
+// const app = require("express")();
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   console.log("access to /");
@@ -12,6 +12,20 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.send("Welcome to my website");
+});
+
+app.get("/desert", (req, res) => {
+  res.send(`
+  <html>
+    <head>
+        <link rel="stylesheet" href="/stylesheets/style.css" />
+    </head>
+    <body>
+        <h1>Desert</h1>
+        <img src="/images/desert.jpeg" />
+    </body>
+  </html>
+  `);
 });
 
 app.get("*", (req, res) => {
