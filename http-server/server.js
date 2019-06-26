362f2faeb8ed9f36e7dc46926b311ce837cfbf8b
mjarraya <mjarraya@student.42.fr>
2019-06-26 11:44:05 +0200

    http-server

--- http-server/server.js
+++ http-server/server.js
@@ -6,12 +6,15 @@ const server = http.createServer((req, res) => {
 
   if (req.url === "/") {
     res.write("<h1>Welcome to the home page</h1>");
+  } else if (req.url === "/team") {
+    res.write("Min, Bruno, Svenja");
   } else if (req.url === "/about") {
-    res.write("Hello! My name is Montasar");
+    res.write("Hello! My name is Montasar!");
   } else {
+    res.statusCode = 404;
     res.write("Uh oh... you're not supposed to be here");
   }
   res.end();
 });
 
-server.listen(3000);
+server.listen(8000);
